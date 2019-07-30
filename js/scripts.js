aa1c575bbd58a20a151ca8ad46532408819931ad
Luis Alberto Garnica López <lagl95@hotmail.com>
2019-07-30 00:56:52 -0500

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#myCarousel").carousel({interval: 2000});
+    $("#carouselButton").click(function(){
+        if($("#carouselButton").children("span").hasClass("fa-pause")){
+            $("myCarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        }else{
+            $("myCarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+});
+
+function openModal(modalName){
+    if(modalName==="r"){
+        $("#reservationModal").modal("toggle");
+    }else{
+        $("#loginModal").modal("toggle");
+    }
+}
